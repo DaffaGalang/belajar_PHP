0727a02aa7ee2abfd1b36a7beed3edea4bfd3b76
DaffaGalang <df.galangp@gmail.com>
2025-09-15 09:35:28 +0800

    senin 15 sep 25

--- koneksi/perpus.php
+++ koneksi/perpus.php
@@ -1,31 +1,31 @@
 <?php
-//use Dom\Mysql;
+    //use Dom\Mysql;
 
-session_start();
+    session_start();
 
-if( !isset($_SESSION["login"])) {
-    header("location: login.php");
-    exit;
-}
+    if( !isset($_SESSION["login"])) {
+        header("location: login.php");
+        exit;
+    }
 
-include("koneksi.php");
+    include("koneksi.php");
 
-//pagination konfigurasi
-$jml_data = 4;
-$jml_buku = count(query("SELECT * FROM perpus"));
-$jml_halaman = ceil($jml_buku / $jml_data);
-$page = ( isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+    //pagination konfigurasi
+    $jml_data = 4;
+    $jml_buku = count(query("SELECT * FROM perpus"));
+    $jml_halaman = ceil($jml_buku / $jml_data);
+    $page = ( isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
 
-$awalData = ( $jml_data * $page ) - $jml_data;
+    $awalData = ( $jml_data * $page ) - $jml_data;
 
 
-$link = query("SELECT * FROM perpus LIMIT $awalData, $jml_data");
+    $link = query("SELECT * FROM perpus LIMIT $awalData, $jml_data");
 
-if (isset($_GET["cari"])) {
-    $link = cari($_GET["serch"]);
-}
+    if (isset($_GET["cari"])) {
+        $link = cari($_GET["serch"]);
+    }
 
-$no = 1;
+    $no = 1;
 
 ?>
 
@@ -37,30 +37,41 @@ $no = 1;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Buku Perpustakaan Ghanesa</title>
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <style>
+        @media print {
+            #navigasi{
+                display: none;
+            }
+        }
+    </style>
+
 </head>
 
 <body class="bg-gray-100 text-gray-800">
     <div class="container mx-auto px-4 py-6">
 
         <!-- Tampilan Tabel Data -->
-        <div class="overflow-x-auto">
+        <div>
             <h2 class="text-3xl font-bold mb-4 flex justify-center px-4 py-4">Data Buku Perpustakaan</h2>
 
             <!-- Button Navigasi -->
             <div class="flex justify-between items-center mb-4">
                 <!-- Navigasi -->
-                <div class="flex gap-2">
+                <div class="flex gap-2" id="navigasi">
                     <a href="logout.php" class="px-2 py-2 bg-purple-800 text-white rounded hover:bg-purple-700"> LogOut ← </a>
-                    <a href="tambahBuku.php" class="px-5 py-2 bg-violet-600 text-white rounded hover:bg-violet-500">Tambah Data Buku</a>
+                    <a href="tambahBuku.php" class="px-3 py-2 bg-violet-600 text-white rounded hover:bg-violet-500">Tambah Data Buku</a>
                 </div>
                 <!-- Pencarian -->
-                <form action="" method="GET" class="flex items-center gap-2 mb-4">
+                <form action="" method="GET" class="flex items-center gap-2 mb-2" id="navigasi">
                     <a href="perpus.php" class="px-2 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"> Refresh ⟳ </a>
-                    <input type="text" name="serch" class="border rounded px-3 py-2" placeholder="Cari data buku..." autocomplete="off" value="<?= htmlspecialchars($_GET['cari'] ?? '') ?>">
+                    <input type="text" name="serch" class="border rounded px-2 py-2" placeholder="Cari data buku..." autocomplete="off" value="<?= htmlspecialchars($_GET['cari'] ?? '') ?>">
                     <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700" name="cari">Cari</button>
                 </form>
             </div>
+        </div>
 
+        <div class="overflow-x-auto">    
             <!-- Tabell Data -->
             <table class="min-w-full bg-white rounded shadow table-fixed">
                 <thead class="bg-gray-200">
--- koneksi/registrasi.php
+++ koneksi/registrasi.php
@@ -15,7 +15,6 @@ if( isset ($_POST["submit"])) {
     } else {
         echo mysqli_error($koneksi);
     }
-
 }
 
 
